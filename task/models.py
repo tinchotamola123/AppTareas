14cafe15a7d9189b78edb3532100a7552f499847
Martin Tamola <tinchotamola123@gmail.com>
2023-04-18 14:13:11 -0300

    Subida de Prpyecto

---
+++ task/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+
+class Task(models.Model):
+    tittle = models.CharField(max_length=100)
+    description = models.TextField(blank=True)
+    creadted = models.DateTimeField(auto_now_add=True)
+    datecompleted = models.DateField(null=True, blank=True)
+    important = models.BooleanField(default=False)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return self.tittle + ' - by ' + self.user.username
+    
+    
